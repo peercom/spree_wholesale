51c8d7e971c7817fcbae3572c868addadb79faa9
Holger von Ameln <holger.von.ameln@peercom.org>
2013-03-27 16:13:03 +0100

    wrestling deface

--- app/overrides/product_wholesale_price.rb
+++ app/overrides/product_wholesale_price.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   :virtual_path => 'spree/products/_cart_form',
   :name => 'product_wholesale_price',
-  :replace_contents => "[data-hook='products_list_item'] div#product-price",
+  :replace_contents => "div#product-price",
   :partial => "spree/hooks/product_wholesale_price",
   :disabled => false
 )
