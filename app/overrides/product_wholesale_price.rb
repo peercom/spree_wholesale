d65a5260fc3379613d6f1101a856a2a78ef51872
Holger von Ameln <holger.von.ameln@peercom.org>
2013-03-27 16:10:32 +0100

    only show wholesale price in product list

--- app/overrides/product_wholesale_price.rb
+++ app/overrides/product_wholesale_price.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   :virtual_path => 'spree/products/_cart_form',
   :name => 'product_wholesale_price',
-  :insert_bottom => "#product-price",
+  :replace_contents => "[data-hook='products_list_item'] div#product-price",
   :partial => "spree/hooks/product_wholesale_price",
   :disabled => false
 )
