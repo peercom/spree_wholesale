94ba55db1ffa9ee7227b8a81f73dd7455017c97c
Holger von Ameln <holger.von.ameln@peercom.org>
2013-03-28 17:44:14 +0100

    admin_orders table

--- app/overrides/admin/admin-orders-headers.rb
+++ app/overrides/admin/admin-orders-headers.rb
@@ -1,6 +1,6 @@
 #insert_after :admin_orders_index_headers, 'admin/hooks/admin_orders_index_headers'
 Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
 :name => 'admin-orders-headers',
-:insert_bottom => "[data-hook='admin_orders_index_headers'], #admin_orders_index_headers[data-hook]",
+:insert_before => "[data-hook='admin_orders_index_headers'], #admin_orders_index_header_actions[data-hook]",
 :partial => "spree/admin/hooks/admin_orders_index_headers",
 :disabled => false)
